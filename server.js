fab799ce9479c9d5880bafd9ac2cf58be06c3016
Christina <Christina@Billys-MacBook-Pro.local>
2016-03-28 20:48:35 -0500

    sign up, login, authentication

--- server.js
+++ server.js
@@ -16,18 +16,35 @@ const SESSION_SECRET = process.env.SESSION_SECRET || 'secret';
 
 app.set('view engine', 'jade');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 app.use(session({
   secret: SESSION_SECRET,
   store: new RedisStore()
 }));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+ // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+ // Set to true if you need the website to include cookies in the requests sent
+ // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+ // Pass to next layer of middleware
+  next();
+});
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.locals = '';
 
-mongoose.connect('mongodb://localhost:27017/nodeauth', (err) => {
+app.use(userRoutes);
+
+mongoose.connect('mongodb://localhost:27017/finalProject', (err) => {
   if (err) throw err;
 
   app.listen(PORT, () => {
